0578b19259a9e72734050bfaf05643ec4e229d46
Carlos Castillo <castillomartinezcarlos@gmail.com>
2021-03-10 16:49:11 +0100

    Nivell 1

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,53 +1,60 @@
-var rockets = [];
+var rockets = []; // Crea un array para almacenar cohetes
 var divShowRockets = document.getElementsByClassName("showRocket")[0];
-var acceleration = 10;
+var acceleration = 10; // Determina la aceleración
+// Función pora imprimir cohete
 function printRocket(rocket) {
     var showRocket = document.createElement("p");
     showRocket.innerHTML = rocket.printRocket();
     divShowRockets.appendChild(showRocket);
 }
-//////////////////////////////// CREAR FUNCTION PARA CREACION DE BOTONES GENERICOS //////////////////////
+// Función pora crear cohete y mostrar sus botones de control e impresión
 function createRocket(code, engines) {
-    var rocket = new Rocket(code, engines);
-    console.log(rocket);
-    rockets.push(rocket);
+    var rocket = new Rocket(code, engines); // Crea instancia de cohete
+    console.log(rocket); // Muestra cohete en consola
+    rockets.push(rocket); // Añade cohete al array de cohetes
     var accelerateRocketsDiv = document.getElementsByClassName("accelerateRockets")[0];
     var printRocketsDiv = document.getElementsByClassName("printRockets")[0];
+    // Crea botón de aceleración
     var accelerateDiv = document.createElement("div");
     var accelerateButton = document.createElement("button");
     accelerateButton.className = "accelerateRocket" + code;
     accelerateButton.innerHTML = "Accelerate Rocket " + code;
     accelerateButton.addEventListener("click", function () {
-        rockets[0].accelerate(acceleration);
+        rocket.accelerate(acceleration);
     });
     accelerateDiv.appendChild(accelerateButton);
+    // Crea botón de freno
     var brakeButton = document.createElement("button");
     brakeButton.className = "brakeRocket" + code;
     brakeButton.innerHTML = "Brake Rocket " + code;
     brakeButton.addEventListener("click", function () {
-        rockets[0].accelerate(-acceleration);
+        rocket.accelerate(-acceleration);
     });
     accelerateDiv.appendChild(brakeButton);
     accelerateRocketsDiv.appendChild(accelerateDiv);
+    // Crea botón de impresión
     var printButton = document.createElement("button");
     printButton.className = "printRocket" + code;
     printButton.innerHTML = "Print Rocket " + code + " info";
     printButton.addEventListener("click", function () {
         divShowRockets.innerHTML = "";
-        printRocket(rockets[0]);
+        printRocket(rocket);
     });
     printRocketsDiv.appendChild(printButton);
 }
+// Crea rocket 1
 var createRocket1Input = document.getElementById("createRocket1");
 createRocket1Input === null || createRocket1Input === void 0 ? void 0 : createRocket1Input.addEventListener("click", function () {
     createRocket("32WESSDS", [10, 30, 80]);
     createRocket1Input.disabled = true;
 });
+// Crea rocket 2
 var createRocket2Input = document.getElementById("createRocket2");
 createRocket2Input === null || createRocket2Input === void 0 ? void 0 : createRocket2Input.addEventListener("click", function () {
     createRocket("LDSFJA32", [30, 40, 50, 50, 30, 10]);
     createRocket2Input.disabled = true;
 });
+// Imprime información de todos los cohetes
 var printAllRocketsInput = document.getElementById("printAll");
 printAllRocketsInput === null || printAllRocketsInput === void 0 ? void 0 : printAllRocketsInput.addEventListener("click", function () {
     divShowRockets.innerHTML = "";
--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -1,73 +1,76 @@
 
-let rockets:Rocket[] = [];
+let rockets:Rocket[] = [];      // Crea un array para almacenar cohetes
 let divShowRockets = document.getElementsByClassName("showRocket")[0] as HTMLDivElement;
-let acceleration:number = 10;
+let acceleration:number = 10;   // Determina la aceleración
 
+// Función pora imprimir cohete
 function printRocket(rocket:Rocket){
     let showRocket = document.createElement("p");
     showRocket.innerHTML = rocket.printRocket();
     divShowRockets.appendChild(showRocket);
-    
 }
 
-//////////////////////////////// CREAR FUNCTION PARA CREACION DE BOTONES GENERICOS //////////////////////
-
+// Función pora crear cohete y mostrar sus botones de control e impresión
 function createRocket(code:string, engines:number[]) {
-    let rocket:Rocket = new Rocket(code, engines);
-    console.log(rocket);
-    rockets.push(rocket);
+    let rocket:Rocket = new Rocket(code, engines);      // Crea instancia de cohete
+    console.log(rocket);        // Muestra cohete en consola
+    rockets.push(rocket);       // Añade cohete al array de cohetes
     
     let accelerateRocketsDiv = document.getElementsByClassName("accelerateRockets")[0];
     let printRocketsDiv = document.getElementsByClassName("printRockets")[0];
     
+    // Crea botón de aceleración
     let accelerateDiv = document.createElement("div");
     let accelerateButton = document.createElement("button");
     accelerateButton.className = "accelerateRocket"+code;
     accelerateButton.innerHTML = "Accelerate Rocket "+code;
     accelerateButton.addEventListener("click", function(){
-        rockets[0].accelerate(acceleration);
+        rocket.accelerate(acceleration);
     });
     accelerateDiv.appendChild(accelerateButton);
 
+    // Crea botón de freno
     let brakeButton = document.createElement("button");
     brakeButton.className = "brakeRocket"+code;
     brakeButton.innerHTML = "Brake Rocket "+code;
     brakeButton.addEventListener("click", function(){
-        rockets[0].accelerate(-acceleration);
+        rocket.accelerate(-acceleration);
     });
     accelerateDiv.appendChild(brakeButton);
     accelerateRocketsDiv.appendChild(accelerateDiv);
 
+    // Crea botón de impresión
     let printButton = document.createElement("button");
     printButton.className = "printRocket"+code;
     printButton.innerHTML = "Print Rocket "+code+" info";
     printButton.addEventListener("click", function(){
         divShowRockets.innerHTML = "";
-        printRocket(rockets[0]);
+        printRocket(rocket);
     });
     printRocketsDiv.appendChild(printButton);
 
 }
 
-let createRocket1Input = document.getElementById("createRocket1");
+// Crea rocket 1
+let createRocket1Input = document.getElementById("createRocket1") as HTMLButtonElement;
 createRocket1Input?.addEventListener("click", function(){
     createRocket("32WESSDS",[10,30,80]);
     createRocket1Input.disabled = true;
 });
 
-
-let createRocket2Input = document.getElementById("createRocket2");
+// Crea rocket 2
+let createRocket2Input = document.getElementById("createRocket2") as HTMLButtonElement;
 createRocket2Input?.addEventListener("click", function(){
     createRocket("LDSFJA32",[30,40,50,50,30,10]);
     createRocket2Input.disabled = true;
 });
 
+// Imprime información de todos los cohetes
 let printAllRocketsInput = document.getElementById("printAll");
 printAllRocketsInput?.addEventListener("click", function(){
     divShowRockets.innerHTML = "";
     
     rockets.forEach(element => printRocket(element));
-    
 });
 
 
